28162351076dc96ff2fb747a3eed085400ff1544
Flavio Poletti <flavio@polettix.it>
2023-03-11 15:04:10 +0100

    1.62

--- lib/Template/Perlish.pm
+++ lib/Template/Perlish.pm
@@ -8,7 +8,7 @@ use strict;
 use Carp;
 use English qw( -no_match_vars );
 use constant ERROR_CONTEXT => 3;
-{ our $VERSION = '1.60'; }
+{ our $VERSION = '1.62'; }
 use Scalar::Util qw< blessed reftype >;
 
 # Function-oriented interface
